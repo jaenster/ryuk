718ee5253456dfdc66a9065afa9ead3dba1c8675
Jan Stoots <github@jaenster.net>
2023-06-09 12:04:34 +0200

    Add resurrect shop action

--- src/lib/town/actions/resurrect.ts
+++ src/lib/town/actions/resurrect.ts
@@ -1,19 +1,71 @@
+import {ShopAction} from "../actions";
+import {NpcFlags} from "../npcs";
+import {Urgency} from "../enums";
+import {ShopTask} from "../task";
+import sdk from "../../../sdk";
 
-/*
+
 new class Resurrect extends ShopAction {
   readonly type: string = 'resurrect';
   readonly npcFlag: number = NpcFlags.RESURRECT;
 
   check(): Urgency {
-    // For now always say its needed
+
+    // @ts-expect-error classic exists
+    if (me.classic || !Config.UseMerc || me.gold < me.mercrevivecost || me.mercrevivecost === 0) {
+      return Urgency.Not;
+    }
+
+    Misc.poll(() => me.gameReady, 1000, 100);
+
+    // me.getMerc() might return null if called right after taking a portal, that's why there's retry attempts
+    const merc = Misc.poll(() => me.getMerc(), 300, 10);
+    if (merc && !merc.dead) {
+      return Urgency.Not;
+    }
+
     return Urgency.Needed;
   }
 
   needTown(task: ShopTask): boolean {
-    return true;
+    return task.urgency === Urgency.Needed;
   }
 
   run(task: ShopTask): boolean {
-    return false;
+    this.goto(task.npc).interact();
+
+    if (!getInteractedNPC()) {
+      return false;
+    }
+
+    for (let i = 0; i < 3; i += 1) {
+      let dialog = getDialogLines();
+      if (dialog) {
+
+        for (let lines = 0; lines < dialog.length; lines += 1) {
+          if (dialog[lines].text.match(/:/gi)) {
+            dialog[lines].handler();
+
+            // Wait foir dialog lines to be gone, or have no text with : in it
+            Misc.poll(() => getDialogLines() === false || !(getDialogLines() as {
+              text: string
+            }[])?.some?.(el => el.text.match(/:/gi)), Math.max(750, me.ping * 3), 10)
+            break;
+          }
+
+          // "You do not have enough gold for that."
+          if (dialog[lines].text.toLocaleLowerCase().match(getLocaleString(sdk.locale.dialog.youDoNotHaveEnoughGoldForThat).toLocaleLowerCase())) {
+            return false;
+          }
+        }
+      }
+
+      const gotMerc = Misc.poll(() => me.getMerc(), Math.max(750, me.ping * 3), 10);
+      if (gotMerc) break;
+    }
   }
-}*/
+
+  dependencies(): string[] {
+    return [];
+  }
+}
